5e947842a8e398e6a61f5070a5ac62692e9af1e6
David Darnes <me@daviddarnes.com>
2021-02-28 12:33:43 +0000

    Increased build speed using eleventy-cache-assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,58 +1,62 @@
-const GhostContentAPI = require("@tryghost/content-api");
+const Cache = require("@11ty/eleventy-cache-assets");
 
 // Get all post data
-const getPosts = async (api) => {
-  const data = await api.posts.browse({
-    include: "tags,authors",
-    limit: "all",
-  });
+const getPosts = async ({ url, key, version }) => {
+  const data = await Cache(
+    `${url}/ghost/api/v3/content/posts/?key=${key}&limit=all&include=tags,authors`,
+    { duration: "1m", type: "json" }
+  );
 
-  return data;
+  return data.posts;
 };
 
 // Get all page data
-const getPages = async (api) => {
-  const data = await api.pages.browse({
-    limit: "all",
-  });
+const getPages = async ({ url, key, version }) => {
+  const data = await Cache(
+    `${url}/ghost/api/v3/content/pages/?key=${key}&limit=all`,
+    { duration: "1m", type: "json" }
+  );
 
-  return data;
+  return data.pages;
 };
 
 // Get all tag data
-const getTags = async (api) => {
-  const data = await api.tags.browse({
-    include: "count.posts",
-    limit: "all",
-  });
+const getTags = async ({ url, key, version }) => {
+  const data = await Cache(
+    `${url}/ghost/api/v3/content/tags/?key=${key}&limit=all&include=count.posts`,
+    { duration: "1m", type: "json" }
+  );
 
-  return data;
+  return data.tags;
 };
 
 // Get all author data
-const getAuthors = async (api) => {
-  const data = await api.authors.browse({
-    include: "count.posts",
-    limit: "all",
-  });
+const getAuthors = async ({ url, key, version }) => {
+  const data = await Cache(
+    `${url}/ghost/api/v3/content/authors/?key=${key}&limit=all&include=count.posts`,
+    { duration: "1m", type: "json" }
+  );
 
-  return data;
+  return data.authors;
 };
 
 // Get all settings data
-const getSettings = async (api) => {
-  const data = await api.settings.browse();
+const getSettings = async ({ url, key, version }) => {
+  const data = await Cache(`${url}/ghost/api/v3/content/settings/?key=${key}`, {
+    duration: "1m",
+    type: "json",
+  });
 
-  return data;
+  return data.settings;
 };
 
-const getContent = async (api) => {
+const getContent = async (params) => {
   return {
-    posts: await getPosts(api),
-    pages: await getPages(api),
-    tags: await getTags(api),
-    authors: await getAuthors(api),
-    settings: await getSettings(api),
+    posts: await getPosts(params),
+    pages: await getPages(params),
+    tags: await getTags(params),
+    authors: await getAuthors(params),
+    settings: await getSettings(params),
   };
 };
 
@@ -64,13 +68,15 @@ module.exports = (
     version,
   }
 ) => {
-  const api = new GhostContentAPI({
-    url: options.url,
-    key: options.key,
-    version: options.version,
-  });
-
-  eleventyConfig.addGlobalData("ghost", async () => await getContent(api));
+  eleventyConfig.addGlobalData(
+    "ghost",
+    async () =>
+      await getContent({
+        url: options.url,
+        key: options.key,
+        verison: options.version,
+      })
+  );
 
   eleventyConfig.addFilter("filterPosts", (posts, key, value) => {
     return posts.filter((post) =>
